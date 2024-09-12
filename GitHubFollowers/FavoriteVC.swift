ed40471fd72e4306199a09d89df884e29ec35073
roman <kamensky.ra@gmail.com>
2024-09-12 23:11:03 +0200

    day1

---
+++ GitHubFollowers/FavoriteVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteVC.swift
+//  GitHubFollowers
+//
+//  Created by roman on 11.09.2024.
+//
+
+import UIKit
+
+class FavoriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by roman on 11.09.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+    }
+   
+
+}
