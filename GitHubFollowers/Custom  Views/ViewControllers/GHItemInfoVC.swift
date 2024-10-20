d22fed09df214188d76cdbb9841a2a3988dcbe92
roman <kamensky.ra@gmail.com>
2024-10-20 17:20:02 +0200

    Superclasses

---
+++ GitHubFollowers/Custom  Views/ViewControllers/GHItemInfoVC.swift
@@ -0,0 +1,58 @@
+//
+//  GHItemInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by roman on 20.10.2024.
+//
+
+import UIKit
+
+class GHItemInfoVC: UIViewController {
+
+    let stackView = UIStackView()
+    let itemInfoOne = GHItemInfoView()
+    let itemInfoTwo = GHItemInfoView()
+    let actionButton = GHButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackground()
+        layoutUI()
+        configureStackView()
+
+    }
+    
+    private func configureBackground() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoOne)
+        stackView.addArrangedSubview(itemInfoTwo)
+    }
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
